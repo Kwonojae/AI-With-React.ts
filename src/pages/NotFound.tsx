aaaa69d9b21b5eceda040274ae8505acea75bd7e
Kwonojae <rnjsdhwo1325@gmail.com>
2023-12-04 13:25:44 +0900

    footer완료

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>에러 페이지 입니다 .</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center gap-6">
+      <div className="text-3xl font-Gothic">🚫 에러 페이지 입니다.🚫</div>
+      <Link className="text-3xl font-Gothic" to="/">
+        🏠 홈 으로
+      </Link>
+    </div>
+  );
 }
